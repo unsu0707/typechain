94416e05042651d4c86c0349fd2ac64bad505950
unsu0707 <unsu0707@gmail.com>
2022-01-05 15:35:06 +0900

    set tsc-watch

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = "unsu", age = 30, gender = "mail";
-const sayHi = (name, age, gender) => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi(name, age, gender));
-//# sourceMappingURL=index.js.map
