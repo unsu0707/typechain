44adbc421e1d7f6643c5bd1e49bbd33632cc0e5b
unsu0707 <unsu0707@gmail.com>
2022-01-05 15:46:45 +0900

    use interface

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
-const name = "unsu",
-    age = 30,
-    gender = "male";
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
 
-const sayHi = (name: string, age: number, gender: string): string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+const person = {
+    name: "unsu",
+    age: 30,
+    gender: "male"
+}
+
+const sayHi = (person: Human): string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(name, age, gender));
+console.log(sayHi(person));
 
 export { };
