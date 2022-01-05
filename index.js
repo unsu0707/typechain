f146c630b9396875491a488011c37e1cf7790778
unsu0707 <unsu0707@gmail.com>
2022-01-05 15:28:42 +0900

    set types in typescript

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "unsu", age = 30, gender = "mail";
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi(name, age, gender));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "unsu",
+    age = 30,
+    gender = "male";
+
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi(name, age, gender));
+
+export { };
